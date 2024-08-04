ad6bd17c6296f526730dba0fc7e5d2527d1ef112
LAKSHMI14M <118215049+LAKSHMI14M@users.noreply.github.com>
2024-08-04 14:03:37 +0530

    Time: 21 ms (24.12%) | Memory: 11.6 MB (59.49%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,4 +1,11 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
+class Solution(object):
+    def sortColors(self, nums):
+        for i in range(len(nums)):
+            swapped=False
+            for j in range(0,len(nums)-i-1):
+                if nums[j]>nums[j+1]:
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
+                    swapped=True
+            if swapped==False:
+                break
         return nums
