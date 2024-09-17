c6061dbb5f7047106f9751c2a8c8ff41d2efea5c
LAKSHMI14M <118215049+LAKSHMI14M@users.noreply.github.com>
2024-09-17 19:30:34 +0530

    Time: 878 ms (57.48%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,9 @@
+SELECT person_name
+FROM (
+    SELECT person_name, weight, turn, 
+           SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+) AS cumulative_weights
+WHERE total_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
